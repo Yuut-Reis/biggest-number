4702fbbe58ea9a46418d8ac12449842466bcecf8
Yuut <ingride.reis.b@gmail.com>
2022-10-16 17:51:29 -0300

    test(function): :test_tube: checks if the IdentifyBiggestNumber function returns the largest number passed to it

--- src/biggest-number.Test/TestBiggestNumber.cs
+++ src/biggest-number.Test/TestBiggestNumber.cs
@@ -1,15 +1,19 @@
 using Xunit;
 using FluentAssertions;
+using BiggestNumber;
 using System;
 
 namespace BiggestNumber.Test;
 
 public class TestBiggestNumber
 {
-    [Theory]
-    [InlineData(0, 0, 0, 0)]
+    [Theory(DisplayName = "Checks if the IdentifyBiggestNumber function returns the largest number passed to it.")]
+    [InlineData(10, 20, 30, 30)]
+    [InlineData(1, 6, 2, 6)]
+    [InlineData(-15, -28, -1, -1)]
     public void TestIdentifyBiggestNumberSucess(int first, int second, int third, int expectedNumber)
     {
-        throw new NotImplementedException();
+        var biggestNumber = BiggestNumber.IdentifyBiggestNumber(first, second, third);
+        biggestNumber.Should().Be(expectedNumber);
     }
 }
